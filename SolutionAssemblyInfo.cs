fabb8a959819cbe06b07ee4217d78045432a5b48
AK CHAVAN <90214281+iamakchavan@users.noreply.github.com>
2024-11-14 20:21:06 +0530

    Add files via upload

---
+++ SolutionAssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+#if DEBUG
+
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyDescription("Debug build, https://github.com/Flow-Launcher/Flow.Launcher")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyDescription("Release build, https://github.com/Flow-Launcher/Flow.Launcher")]
+#endif
+
+[assembly: AssemblyCompany("Flow Launcher")]
+[assembly: AssemblyProduct("Flow Launcher")]
+[assembly: AssemblyCopyright("The MIT License (MIT)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
